0c29607992ae869ccf412e2ac8e68854c7d19ac5
Atthavit <attawitt@live.com>
2018-09-21 16:54:22 +0700

    add logo

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ const word = "hello-react";
 class App extends Component{
   render(){
     return(
-      <div>
+      <div className="App">
+        <img src="logo.svg" className="App-logo" alt="logo" />
         <WordCard value={word}/>
       </div>
     );
