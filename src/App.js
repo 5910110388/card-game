d4313a3baf733388f91a3ace724ac053f9a59301
Atthavit <attawitt@live.com>
2018-09-21 17:00:06 +0700

    apply "App-logo-fix" class to display logo

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const word = "hello-react";
 class App extends Component{
   render(){
     return(
-      <div className="App">
+      <div className="App-logo-fix">
         <img src="logo.svg" className="App-logo" alt="logo" />
         <WordCard value={word}/>
       </div>
